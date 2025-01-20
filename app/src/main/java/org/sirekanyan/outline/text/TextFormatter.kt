d5e6255a23f47156940e79578b2ff9f20e351f17
Mikhail Savichev <mikhail@savichev.pro>
2025-01-21 01:26:49 +0400

    Added decimal in traffic counter.

--- app/src/main/java/org/sirekanyan/outline/text/TextFormatter.kt
+++ app/src/main/java/org/sirekanyan/outline/text/TextFormatter.kt
@@ -2,10 +2,14 @@ package org.sirekanyan.outline.text
 
 fun formatTraffic(bytes: Long): String =
     when {
-        bytes > 1_000_000_000 -> "${bytes / 1000 / 1000 / 1000} GB"
-        bytes > 1_000_000 -> "${bytes / 1000 / 1000} MB"
-        bytes > 1_000 -> "${bytes / 1000} kB"
-        else -> "$bytes B"
+        bytes > 1_000_000_000 ->
+            "${bytes / 1000 / 1000 / 1000}.${bytes % 1_000_000_000 / 1_000_000 } GB"
+        bytes > 1_000_000 ->
+            "${bytes / 1000 / 1000}.${bytes % 1_000_000 / 1000} MB"
+        bytes > 1_000 ->
+            "${bytes / 1000}.${bytes % 1000} kB"
+        else ->
+            "$bytes B"
     }
 
 fun formatCount(count: Long): String =
